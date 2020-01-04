f3307762ea454e51bd853a98d9b14f41e61405b4
Daniel Menet <membership@sontags.ch>
2020-01-04 22:55:22 +0100

    version: subcommand added

---
+++ cli.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+type App struct {
+	cfg config
+
+	// entry point
+	Execute func() error
+}
+
+func NewApp() *App {
+	a := &App{}
+
+	// root
+	rootCmd := &cobra.Command{
+		Use:   "svck",
+		Short: "Runs http requests and checks the responses",
+	}
+	rootCmd.PersistentFlags().StringVarP(&a.cfg.Address, "address", "a", "", "fake address of server to test")
+	rootCmd.PersistentFlags().StringVarP(&a.cfg.Proto, "proto", "p", "", "fake protocol of server to test")
+	rootCmd.PersistentFlags().StringVar(&a.cfg.UserAgent, "user-agent", "svck", "user agent string to be sent in requests")
+	rootCmd.PersistentFlags().StringVarP(&a.cfg.Output, "output", "o", "default", "output format, can be 'default' or 'json'")
+	a.Execute = rootCmd.Execute
+
+	// run
+	runCmd := &cobra.Command{
+		Use:   "run",
+		Short: "Run checks",
+		Args:  cobra.MinimumNArgs(1),
+		Run:   a.runCmd,
+	}
+	runCmd.PersistentFlags().IntVarP(&a.cfg.Workers, "workers", "w", 3, "number of workers to execute requests concurrently")
+	runCmd.PersistentFlags().IntVarP(&a.cfg.Timeout, "timeout", "t", 10, "number of seconds until a single request runs in a timeout")
+	runCmd.PersistentFlags().BoolVar(&a.cfg.NoProgress, "no-progress", false, "do not display progess bar")
+	rootCmd.AddCommand(runCmd)
+
+	// curl
+	var curlCmd = &cobra.Command{
+		Use:   "curl",
+		Short: "Print all checks as curl commands",
+		Args:  cobra.MinimumNArgs(1),
+		Run:   a.curlCmd,
+	}
+	curlCmd.PersistentFlags().BoolVar(&a.cfg.NoBashComments, "no-bash-comments", false, "do not print bash comments")
+	rootCmd.AddCommand(curlCmd)
+
+	// version
+	versionCmd := &cobra.Command{
+		Use:   "version",
+		Short: "Print version info",
+		Run:   a.versionCmd,
+	}
+	rootCmd.AddCommand(versionCmd)
+
+	return a
+}
+
+func (a App) runCmd(cmd *cobra.Command, args []string) {
+	a.cfg.ServiceFiles = args
+	err := a.cfg.ReadServiceFiles()
+	if err != nil {
+		panic(err)
+	}
+
+	checks, err := NewChecks(a.cfg.services, a.cfg.Address, a.cfg.Proto, a.cfg.UserAgent, a.cfg.Timeout)
+	if err != nil {
+		panic(err)
+	}
+	p := NewPool(checks, a.cfg.Workers)
+	p.Run(!a.cfg.NoProgress)
+
+	success := 0
+	fail := 0
+	if a.cfg.Output == "json" {
+		out, err := json.MarshalIndent(checks, "", "  ")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(out))
+	} else {
+		for _, check := range checks {
+			if check.success {
+				success++
+			} else {
+				fail++
+				fmt.Printf("Failed check: %s\n\tURL: %s\n\tREQUEST_HEADERS: %s\n\tRESPONSE_HEADERS: %s\n\tREPRODUCE: %s\n\tREASON: %s\n\n", check.name(), check.request.URL.String(), check.requestHeaders(""), check.responseHeaders(""), check.asCurl(), check.reason)
+			}
+		}
+
+		fmt.Printf("Summary:\n\tFailed: %d\n\tSuccessful: %d\n", fail, success)
+	}
+}
+
+func (a App) curlCmd(cmd *cobra.Command, args []string) {
+	a.cfg.ServiceFiles = args
+	err := a.cfg.ReadServiceFiles()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	checks, err := NewChecks(a.cfg.services, a.cfg.Address, a.cfg.Proto, a.cfg.UserAgent, a.cfg.Timeout)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if !a.cfg.NoBashComments {
+		fmt.Printf("#!/bin/bash\n")
+	}
+	for _, check := range checks {
+		if !a.cfg.NoBashComments {
+			fmt.Printf("\n# %s\n", check.name())
+		}
+		fmt.Sprintf("%s\n", check.asCurl())
+	}
+}
+
+func (a App) versionCmd(cmd *cobra.Command, args []string) {
+	fmt.Println(versionInfo())
+}
--- main.go
+++ main.go
@@ -1,108 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
-
-	"github.com/spf13/cobra"
 )
-
-var (
-	cfg config
-)
-
-func init() {
-	rootCmd.PersistentFlags().StringVarP(&cfg.Address, "address", "a", "", "fake address of server to test")
-	rootCmd.PersistentFlags().StringVarP(&cfg.Proto, "proto", "p", "", "fake protocol of server to test")
-	rootCmd.PersistentFlags().StringVar(&cfg.UserAgent, "user-agent", "svck", "user agent string to be sent in requests")
-	rootCmd.PersistentFlags().StringVarP(&cfg.Output, "output", "o", "default", "output format, can be 'default' or 'json'")
-
-	rootCmd.AddCommand(runCmd)
-	runCmd.PersistentFlags().IntVarP(&cfg.Workers, "workers", "w", 3, "number of workers to execute requests concurrently")
-	runCmd.PersistentFlags().IntVarP(&cfg.Timeout, "timeout", "t", 10, "number of seconds until a single request runs in a timeout")
-	runCmd.PersistentFlags().BoolVar(&cfg.NoProgress, "no-progress", false, "do not display progess bar")
-
-	rootCmd.AddCommand(curlCmd)
-	curlCmd.PersistentFlags().BoolVar(&cfg.NoBashComments, "no-bash-comments", false, "do not print bash comments")
-}
-
-var rootCmd = &cobra.Command{
-	Use:   "svck",
-	Short: "Runs http requests and checks the responses",
-}
-
-var runCmd = &cobra.Command{
-	Use:   "run",
-	Short: "Run checks",
-	Args:  cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		cfg.ServiceFiles = args
-		err := cfg.ReadServiceFiles()
-		if err != nil {
-			panic(err)
-		}
-
-		checks, err := NewChecks(cfg.services, cfg.Address, cfg.Proto, cfg.UserAgent, cfg.Timeout)
-		if err != nil {
-			panic(err)
-		}
-		p := NewPool(checks, cfg.Workers)
-		p.Run(!cfg.NoProgress)
-
-		success := 0
-		fail := 0
-		if cfg.Output == "json" {
-			out, err := json.MarshalIndent(checks, "", "  ")
-			if err != nil {
-				panic(err)
-			}
-			fmt.Println(string(out))
-		} else {
-			for _, check := range checks {
-				if check.success {
-					success++
-				} else {
-					fail++
-					fmt.Printf("Failed check: %s\n\tURL: %s\n\tREQUEST_HEADERS: %s\n\tRESPONSE_HEADERS: %s\n\tREPRODUCE: %s\n\tREASON: %s\n\n", check.name(), check.request.URL.String(), check.requestHeaders(""), check.responseHeaders(""), check.asCurl(), check.reason)
-				}
-			}
-
-			fmt.Printf("Summary:\n\tFailed: %d\n\tSuccessful: %d\n", fail, success)
-		}
-	},
-}
-
-var curlCmd = &cobra.Command{
-	Use:   "curl",
-	Short: "Print all checks as curl commands",
-	Args:  cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		cfg.ServiceFiles = args
-		err := cfg.ReadServiceFiles()
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		checks, err := NewChecks(cfg.services, cfg.Address, cfg.Proto, cfg.UserAgent, cfg.Timeout)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		if !cfg.NoBashComments {
-			fmt.Printf("#!/bin/bash\n")
-		}
-		for _, check := range checks {
-			if !cfg.NoBashComments {
-				fmt.Printf("\n# %s\n", check.name())
-			}
-			fmt.Sprintf("%s\n", check.asCurl())
-		}
-	},
-}
 
 func main() {
-	if err := rootCmd.Execute(); err != nil {
+	if err := NewApp().Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
 	}
